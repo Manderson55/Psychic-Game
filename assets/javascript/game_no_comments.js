4cbc95f16e2ca7a282e7db475801bd019cf01a97
Manderson55 <mariela.z.anderson@gmail.com>
2017-05-18 17:44:58 -0600

    latest changes

--- assets/javascript/game_no_comments.js
+++ assets/javascript/game_no_comments.js
@@ -5,40 +5,23 @@ var lossCount = 0;
 var guessesLeft = 9;
 var guessedLetters = []; /*don't know how to show this on page */
 
-var userGuess = null;
-
-/* set a function to generate a random letter */
 
 var alphabet = "abcdefghiklmnopqrstuvwxyz";
 var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-console.log("computer guess " + computerGuess);
-
-/* When user presses a key it should run the function */ 
 
 document.onkeyup = function(event) {
-
-/* set userGuess to the key the user pressed */
 	var userGuess = event.key;
 	guessedLetters.push(userGuess);
-
-
-
-
-
-	console.log("user guess " + userGuess);
-
 
 
 	if (guessesLeft > 0) {
 		console.log("guessesLeft is " + guessesLeft);
 		guessesLeft--; 
-		/* subtract 1 from guessesLeft and print it on page*/
 
 		var guessElem = document.getElementById("guessesLeft");
 		guessElem.innerHTML = "Guesses Left: " + guessesLeft;
 
-		/*  print userGuess on page  */	
 		var guessElem = document.getElementById("userGuesses");
 		guessElem.innerHTML = "Your letters for this game: " + userGuess + (', ');	
 		
@@ -49,24 +32,17 @@ document.onkeyup = function(event) {
 			winElem.innerHTML = "Wins: " + wins;
 
 			alert("You guessed it! You have Psychic Powers! The letter you and the computer picked was " + userGuess);
-			console.log("You won!");
-
-/*  update the page with added win and reset number of guesses left and letters picked 		*/	
 
 			var guessElem = document.getElementById("guessesLeft");
 			guessElem.innerHTML = "Guesses Left: 9" ;
 			var guessElem = document.getElementById("userGuesses");
 			guessElem.innerHTML = "Your letters for this game: "; 
 
-/*    update screen with wins    */
-/*    update screen clearing the guessed letters */
+
 			guessesLeft = 9;
 			guessedLetters =[];
 			computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-			console.log("Wins: " + wins)
-			console.log (" Losses: " + losses);
-			console.log (" GuessesLeft: " + guessesLeft); 
-			console.log (" Computer picked next: " + computerGuess);
+
 
 			} 
 
@@ -74,10 +50,7 @@ document.onkeyup = function(event) {
 
 	else  {
 			losses++;
-/* subtract 1 from guessesLeft and print it on page*/
-	
-			console.log(guessesLeft);
-			console.log("You lost!");
+
 			alert("You lost! You DON'T have Psychic Powers!");
 
 			var lossElem = document.getElementById("lossCount");
@@ -88,15 +61,9 @@ document.onkeyup = function(event) {
 			var guessElem = document.getElementById("userGuesses");
 			guessElem.innerHTML = "Your letters for this game: ";
 
-
-/*    update screen clearing the guessed letters */
 			guessedLetters =[];
 			computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-			console.log("Wins: " + wins)
-			console.log (" Losses: " + losses);
-			console.log (" GuessesLeft: " + guessesLeft); 
-			console.log (" Computer picked next: " + computerGuess);
-		
+
 			}
 
 		} 
